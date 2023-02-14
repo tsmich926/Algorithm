f8da9be21c3012b1cffaca4f56c9794c5e8f9a2e
Yeoni <siunaus224@gmail.com>
2023-02-14 20:21:43 +0900

    230214-3

--- 230216/오연주/1259 팰린드롬수.py
+++
@@ -1,8 +0,0 @@
-while True:  # 계속해서 입력됨
-    num = str(input()) # 숫자를 str형태로 입력
-    if num == '0':  # 0이 나오면 반복문 끝남
-        break
-    elif num == num[::-1]:
-        print('yes')
-    elif num != num[::-1]:
-        print('no')
--- 230216/오연주/2456 나는 학급회장이다.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-
-sumA = [0] * 4
-sumB = [0] * 4
-sumC = [0] * 4
-for tc in range(T):
-    A, B, C = map(int, input().split())
-    sumA[A]
