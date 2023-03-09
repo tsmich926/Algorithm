9dbf1f9fa6cda57ec2e82a242f98dade90cef28f
bumjin <bumjin4u@naver.com>
2023-03-09 19:39:28 +0900

    0309

--- 230209/박범진/3040 백설 공주와 일곱 난쟁이.py
+++ 230209/박범진/3040 백설 공주와 일곱 난쟁이.py
@@ -1,9 +1,9 @@
 def seven(arr, sumV):
     for i in range(0, 8):
         for j in range(i+1, 9):
-            if arr[i] + arr[j] == sumV - 100:
-                f_lst.append(arr[i])
-                f_lst.append(arr[j])
+            if arr[i] + arr[j] == sumV - 100:    # 모든 난쟁이를 더한 값에서 100을 빼면 가짜 난쟁이 둘의 값이 나옴
+                f_lst.append(arr[i])    # 가짜 난쟁이1 가짜리스트에 추가
+                f_lst.append(arr[j])    # 가짜 난쟁이2 가짜 리스트에 추가
                 return
 
 arr = [int(input()) for _ in range(9)]
@@ -14,7 +14,7 @@ ans = []
 seven(arr, sumV)
 
 for i in arr:
-    if i not in f_lst:
+    if i not in f_lst:    # 가짜 난쟁이 둘을 제외한 나머지 난쟁이들 정답 리스트에 추가
         ans.append(i)
 ans = sorted(ans)
 
